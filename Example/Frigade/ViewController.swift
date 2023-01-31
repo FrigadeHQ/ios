f93c1813ea88d47a56d74c60cd54055dbf69d202
christianmat <christian_mathiesen@alumni.brown.edu>
2023-01-31 12:24:15 -0800

    Initial commit

---
+++ Example/Frigade/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Frigade
+//
+//  Created by christianmat on 01/31/2023.
+//  Copyright (c) 2023 christianmat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
