24f87368c2b45de6a06330978a7fec2aba85a3bf
Jacob Schwartz <jacob@omgpanda.com>
2023-02-06 01:03:23 -0800

    recognize HTTP status code errors

--- Frigade/Classes/Network/Agent.swift
+++ Frigade/Classes/Network/Agent.swift
@@ -12,11 +12,16 @@ struct Agent {
     struct WrappedData: Codable {
         let data: String
     }
-
+    
     func run<T: Decodable>(_ request: URLRequest, _ decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<Response<T>, Error> {
         return URLSession.shared
             .dataTaskPublisher(for: request)
             .tryMap { result -> Response<T> in
+                guard let httpResponse = result.response as? HTTPURLResponse,
+                      httpResponse.statusCode == 200 || httpResponse.statusCode == 201 else {
+                    throw URLError(.badServerResponse)
+                }
+                
                 let wrapped = try decoder.decode(WrappedData.self, from: result.data)
                 let value = try decoder.decode(T.self, from: wrapped.data.data(using: .utf8) ?? Data())
                 return Response(value: value, response: result.response)
